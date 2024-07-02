7ee8916c8c42a670e6b15a49e1efc026cb02d9e6
shreyaan <shreyaan.saha.1009@gmail.com>
2024-07-02 15:28:08 +0530

    changed MySkills

---
+++ src/skills.js
@@ -0,0 +1,80 @@
+export const skills=[
+        {
+            id : 0,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710752678/Portfolio/download_nqtrhc.png",
+            title : 'Data Structures & Algorithms',
+            style : 'shadow-gray-400'
+        },
+        {
+            id : 1,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749482/Portfolio/html_uipxob.png",
+            title : 'HTML',
+            style : 'shadow-orange-500'
+        },
+        {
+            id : 2,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749482/Portfolio/css_fd5ueg.png",
+            title : 'CSS',
+            style : 'shadow-sky-700'
+        },
+        {
+            id : 3,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749484/Portfolio/js_bfsnyq.png",
+            title : 'JavaScript',
+            style : 'shadow-yellow-500'
+        },
+        {
+            id : 4,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749482/Portfolio/react_hibgwv.png",
+            title : 'ReactJS',
+            style : 'shadow-sky-500'
+        },
+        {
+            id : 5,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749482/Portfolio/tailwind_vrco96.png",
+            title : 'Tailwind',
+            style : 'shadow-cyan-500'
+        },
+        {
+            id : 6,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749482/Portfolio/gh_orxysy.png",
+            title : 'GitHub',
+            style : 'shadow-gray-400'
+        },
+        {
+            id : 7,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710749483/Portfolio/bootstrap_rwlagn.png",
+            title : 'Bootstrap',
+            style : 'shadow-purple-500'
+        },
+        {
+            id : 8,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710754166/Portfolio/mongodb_original_logo_icon_146424_xtqrv9.png",
+            title : 'MongoDB',
+            style : 'shadow-green-500'
+        },
+        {
+            id : 9,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710754662/Portfolio/pngwing.com_shyxmt.png",
+            title : 'ExpressJS',
+            style : 'shadow-gray-500'
+        },
+        {
+            id : 10,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710754658/Portfolio/pngegg_hlqfd2.png",
+            title : 'NodeJS',
+            style : 'shadow-green-500'
+        },
+        {
+            id : 11,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710755563/Portfolio/800px-Python-logo-notext.svg_xh3fhk.png",
+            title : 'Python',
+            style : 'shadow-yellow-500'
+        },
+        {
+            id : 12,
+            src : "https://res.cloudinary.com/deqvxfp7f/image/upload/v1710755719/Portfolio/e8544455f56ab1d668521bead398f84a_pm32wk.png",
+            title : 'Machine Learning',
+            style : 'shadow-sky-500'
+        },
+    ]
