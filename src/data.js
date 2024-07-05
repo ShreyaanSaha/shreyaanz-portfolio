3009147889690b9f02354153c77ab7d634ff19f8
shreyaan <shreyaan.saha.1009@gmail.com>
2024-07-05 07:43:46 +0530

    updated data

--- src/data.js
+++ src/data.js
@@ -23,4 +23,12 @@ export const projects = [
       image: "https://res.cloudinary.com/deqvxfp7f/image/upload/v1708673862/Projects/Screenshot_2024-02-23_130624_ro9bj6.png",
       link: "https://github.com/ShreyaanSaha/Multiple-Disease-Prediction-App",
     },
+    {
+      title: "Forex Analysis and Forecasting",
+      subtitle: "Python, Time Series Analysis",
+      description:
+        "This is a comprehensive project for Forex exchange rate analysis and forecasting using advanced time series techniques implemented in Python. The system will focus on predicting the exchange rates of five major currencies: Euro (EUR), Japanese Yen (JPY), Indian Rupees (INR), UK Pound Sterling (GBP), and Chinese Yuan (CNY). Utilizing the power of ARIMA, SARIMA, and SARIMAX models, the application will analyze historical exchange rate data to provide accurate forecasts, aiding in investment decisions and risk management.",
+      image: "https://res.cloudinary.com/deqvxfp7f/image/upload/v1720145149/Projects/Forex_Analysis_and_Forecasting_1_bpg0e5.png",
+      link: "https://github.com/ShreyaanSaha/Forex-Analysis-and-Forecasting",
+    },
   ];
